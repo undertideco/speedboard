c56f6b1e3f0e123f16ff720cc2245f4bf31331e6
Jurvis Tan <jurvis.tan@me.com>
2020-04-20 15:21:14 -0700

    spike configuration view

---
+++ SpeedLaunch/ConfigurationView.swift
@@ -0,0 +1,39 @@
+//
+//  ConfigurationView.swift
+//  SpeedLaunch
+//
+//  Created by Jurvis Tan on 20/4/20.
+//  Copyright © 2020 Undertide LLP. All rights reserved.
+//
+
+import SwiftUI
+import Contacts
+
+struct ConfigurationView: View {
+    @State private var user = ""
+    @State private var isShowingContactSelector = false
+    @State private var selectedContact: CNContact? = nil
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Button(action: {
+                    self.isShowingContactSelector = true
+                }) { Text("Select a Contact") }
+                
+            }.navigationBarTitle(Text("Configure Action"))
+        }.sheet(isPresented: $isShowingContactSelector) {
+            EmbeddedContactPicker(didSelectContact: { contact in
+                self.selectedContact = contact
+            }) {
+                self.isShowingContactSelector = false
+            }
+        }
+    }
+}
+
+struct ConfigurationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConfigurationView()
+    }
+}
