f2c657c967a50ba0223865d716bc050ba7ad2a06
Jurvis Tan <jurvis.tan@me.com>
2020-04-27 16:47:10 -0700

    spike ShortcutImageView

---
+++ SpeedLaunch/ShortcutImageView.swift
@@ -0,0 +1,35 @@
+//
+//  ShortcutImageView.swift
+//  SpeedLaunch
+//
+//  Created by Jurvis Tan on 27/4/20.
+//  Copyright © 2020 Undertide LLP. All rights reserved.
+//
+
+import SwiftUI
+import ContactsUI
+
+struct ShortcutImageView: View {
+    @Binding var type: ActionType
+    var image: UIImage
+    
+    var body: some View {
+        ZStack(alignment: Alignment(horizontal: .trailing, vertical: .top)) {
+            Image(uiImage: image)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 75, height: 75)
+                .mask(Circle())
+            
+            if type == .call {
+                Image("call_glyph")
+                    .foregroundColor(.green)
+            } else if type == .message {
+                Image("message_glyph")
+                    .foregroundColor(.orange)
+            } else {
+                Image("gallery_glyph")
+            }
+        }
+    }
+}
