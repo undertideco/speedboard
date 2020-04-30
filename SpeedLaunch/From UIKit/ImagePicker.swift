7ff31fba0b4e07231195eeacc6853a0b42aa2fe8
Jurvis Tan <jurvis.tan@me.com>
2020-04-29 17:05:39 -0700

    make representable UIImagePickerVC

---
+++ SpeedLaunch/From UIKit/ImagePicker.swift
@@ -0,0 +1,44 @@
+//
+//  ImagePicker.swift
+//  SpeedLaunch
+//
+//  Created by Jurvis Tan on 29/4/20.
+//  Copyright © 2020 Undertide LLP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Environment(\.presentationMode) var presentationMode
+    @Binding var image: UIImage
+    
+    final class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiIimage = info[.originalImage] as? UIImage {
+                parent.image = uiIimage
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+
+    }
+}
