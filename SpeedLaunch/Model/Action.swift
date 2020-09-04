0e2884bd18a926ca8ab744f64c02ad16ce7e6574
Jurvis Tan <jurvis.tan@me.com>
2020-09-04 14:25:22 -0700

    add action support for facetime

--- SpeedLaunch/Model/Action.swift
+++ SpeedLaunch/Model/Action.swift
@@ -17,39 +17,30 @@ struct Action: Codable, Equatable {
     
     func generateURLLaunchSchemeString() -> URL? {
         let phoneNumberKit = PhoneNumberKit()
-
+        
+        guard let phoneNumber = phoneNumber else { return nil }
+        let parsedNumber = try! phoneNumberKit.parse(phoneNumber, ignoreType: true)
+        var actionComponents = URLComponents(string: phoneNumberKit.format(parsedNumber, toType: .e164))!
+        
         switch type {
         case .message:
-            guard let phoneNumber = phoneNumber else { return nil }
-            let parsedNumber = try! phoneNumberKit.parse(phoneNumber, ignoreType: true)
-            var actionComponents = URLComponents(string: phoneNumberKit.format(parsedNumber, toType: .e164))!
             actionComponents.scheme = "sms"
-            
-            var wrappedComponents = URLComponents()
-            wrappedComponents.scheme = "speedboard"
-            wrappedComponents.path = "/open"
-            wrappedComponents.queryItems = [
-                URLQueryItem(name: "url", value: "\(actionComponents.url!.absoluteString)")
-            ]
-            
-            return wrappedComponents.url!
         case .call:
-            guard let phoneNumber = phoneNumber else { return nil }
-            let parsedNumber = try! phoneNumberKit.parse(phoneNumber, ignoreType: true)
-            var actionComponents = URLComponents(string: phoneNumberKit.format(parsedNumber, toType: .e164))!
             actionComponents.scheme = "tel"
-            
-            var wrappedComponents = URLComponents()
-            wrappedComponents.scheme = "speedboard"
-            wrappedComponents.path = "/open"
-            wrappedComponents.queryItems = [
-                URLQueryItem(name: "url", value: "\(actionComponents.url!.absoluteString)")
-            ]
-            
-            return wrappedComponents.url!
+        case .facetime:
+            actionComponents.scheme = "facetime"
         default:
-            return nil
+            break
         }
+        
+        var wrappedComponents = URLComponents()
+        wrappedComponents.scheme = "speedboard"
+        wrappedComponents.path = "/open"
+        wrappedComponents.queryItems = [
+            URLQueryItem(name: "url", value: "\(actionComponents.url!.absoluteString)")
+        ]
+        
+        return wrappedComponents.url
     }
 }
 
