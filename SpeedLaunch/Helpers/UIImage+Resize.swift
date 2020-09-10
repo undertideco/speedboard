a0a6ace352a22f29bf398be3c1016af175a89506
Jurvis Tan <jurvis.tan@me.com>
2020-09-10 15:46:32 -0700

    add extension to resize images

---
+++ SpeedLaunch/Helpers/UIImage+Resize.swift
@@ -0,0 +1,40 @@
+//
+//  UIImage+Resize.swift
+//  SpeedLaunch
+//
+//  Created by Jurvis Tan on 9/10/20.
+//  Copyright © 2020 Undertide LLP. All rights reserved.
+//
+
+import UIKit
+
+extension UIImage {
+    class func resize(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        
+        let widthRatio  = targetSize.width  / image.size.width
+        let heightRatio = targetSize.height / image.size.height
+        
+        var newSize: CGSize
+        if widthRatio > heightRatio {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+        }
+        
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 0)
+        image.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage!
+    }
+    
+    class func scale(image: UIImage, by scale: CGFloat) -> UIImage? {
+        let size = image.size
+        let scaledSize = CGSize(width: size.width * scale, height: size.height * scale)
+        return UIImage.resize(image: image, targetSize: scaledSize)
+    }
+}
